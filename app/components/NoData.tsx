134f6bd476200cff397c2b42fdf097cc8dde2aa8
sohaekim <sohaekim1@gmail.com>
2025-04-15 23:26:04 +0900

    update: png -> webp

--- app/components/NoData.tsx
+++ app/components/NoData.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface Props {
   message: string;
 }
@@ -7,7 +5,7 @@ interface Props {
 export default function NoData({ message }: Props) {
   return (
     <div className="flex flex-col gap-[24px] items-center justify-center py-[80px]">
-      <img className="w-[80px] h-[80px]" src="/icon_book.png" alt="no-data" />
+      <img className="w-[80px] h-[80px]" src="/book.webp" alt="no-data" />
       <p className="caption text-txt-secondary">{message}</p>
     </div>
   );
