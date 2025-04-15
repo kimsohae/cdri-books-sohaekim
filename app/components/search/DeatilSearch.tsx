1f5d779d3ea4274a993d8c6949a843c9e8f6fd1e
sohaekim <sohaekim1@gmail.com>
2025-04-15 15:36:33 +0900

    add: search - 검색, 상세검색, 검색아이콘

---
+++ app/components/search/DeatilSearch.tsx
@@ -0,0 +1,157 @@
+import React, { useRef, useState } from "react";
+import Button from "@/components/Button";
+import CancelIcon from "@/components/icon/CancelIcon";
+import Spacing from "@/components/Spacing";
+import { useClickOutside } from "@/lib/hooks";
+import ArrowIcon from "../icon/ArrowIcon";
+
+const SEARCH_OPTIONS: Option[] = [
+  {
+    label: "제목",
+    value: "title",
+  },
+  {
+    label: "저자명",
+    value: "person",
+  },
+  {
+    label: "출판사",
+    value: "publisher",
+  },
+];
+
+export type Option = {
+  label: string;
+  value: string;
+};
+
+interface Props {
+  options?: Option[];
+  handleSearch: (searchOption: { target: string; query: string }) => void;
+  placeholder: string;
+}
+
+/**
+ * 상세 검색 컴포넌트
+ *
+ * 검색 옵션 dropdown, 검색어 input, 검색 버튼
+ * @param dropdown 구성할 옵션 목록
+ * @param handleSearch  검색 실행 함수
+ * @param placeholder   검색어 input 플레이스홀더
+ */
+export default function DetailSearch({
+  options = SEARCH_OPTIONS,
+  handleSearch,
+  placeholder,
+}: Props) {
+  const searchValueRef = useRef<HTMLInputElement>(null);
+  const popupRef = useRef<HTMLDivElement>(null);
+  const [option, setOption] = useState<Option>(options[0]);
+  const [isPopupOpened, setIsPopupOpened] = useState<boolean>(false);
+  const [isOptionOpened, setIsOptionOpened] = useState<boolean>(false);
+
+  /**
+   * 검색 버튼 ClickEventHandler
+   * @description searchValueRef 값을 읽고, 값이 있는 경우 target과 함께 검색
+   */
+  const onClickSearch = () => {
+    const serchWord = searchValueRef.current?.value;
+    if (serchWord) {
+      const searchOption = {
+        target: option.value,
+        query: serchWord,
+      };
+      handleSearch(searchOption);
+    }
+  };
+
+  /** 팝업 토글 : 검색옵션도 함께 닫음*/
+  const togglePopup = () => {
+    setIsPopupOpened(!isPopupOpened);
+    setIsOptionOpened(false);
+  };
+
+  /** 검색옵션 토글  */
+  const toggleOption = () => {
+    setIsOptionOpened(!isOptionOpened);
+  };
+
+  useClickOutside(popupRef, () => {
+    setIsPopupOpened(false);
+  });
+
+  return (
+    <div className="relative z-[1]">
+      <Button
+        size="sm"
+        color="lightGray"
+        variant="outline"
+        onClick={togglePopup}
+      >
+        상세검색
+      </Button>
+      {/** 팝업 오픈 시 상세검색 토글(useClickOutside side effect 방지) */}
+      {isPopupOpened && (
+        <div className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
+      )}
+      <div
+        ref={popupRef}
+        className={`absolute mt-[14px] left-1/2 -translate-x-1/2  px-[24px] py-[36px] bg-white rounded-default shadow-md ${
+          isPopupOpened ? "block" : "hidden"
+        }`}
+      >
+        <div className="flex gap-1">
+          <button
+            className="absolute top-[12px] right-[12px]"
+            onClick={togglePopup}
+          >
+            <CancelIcon />
+          </button>
+          <div className="relative">
+            <button
+              className="w-[100px] h-[30px] py-[4px] px-[8px] flex items-center justify-between border-b border-ghostGray text-txt-primary font-bold text-[14px] text-left"
+              onClick={toggleOption}
+            >
+              <span>{option.label || placeholder}</span>
+
+              <ArrowIcon className="w-[10px] h-[10px] " />
+            </button>
+            {isOptionOpened && (
+              <div className="absolute bottom-[-64px] w-[100px] flex flex-col absolute  body2 text-txt-subtitle bg-white shadow-sm ">
+                {options.map((el) => {
+                  if (el.value === option.value) {
+                    return <React.Fragment key={el.value} />;
+                  }
+                  return (
+                    <button
+                      key={el.value}
+                      onClick={() => {
+                        setOption(el);
+                        toggleOption();
+                      }}
+                      className="py-[4px] px-[8px] text-left "
+                    >
+                      {el.label}
+                    </button>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          {/* <div className=""> */}
+          <input
+            data-input="detail"
+            className="focus:outline-none cpation w-[208px] border-b border-primary body2 text-[#8D94A0] p-[4px]"
+            ref={searchValueRef}
+            placeholder={placeholder}
+          />
+          {/* </div> */}
+        </div>
+        <Spacing h={16} />
+        <Button size="sm" onClick={onClickSearch}>
+          검색하기
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/search/Search.tsx
@@ -0,0 +1,156 @@
+import React, { useRef, useState } from "react";
+import { LocalStorageUtility } from "@/lib/utils";
+import { SEARCH_HISTORY } from "@/lib/constants";
+import { useClickOutside } from "@/lib/hooks";
+import SearchIcon from "@/components/icon/SearchIcon";
+import CancelIcon from "@/components/icon/CancelIcon";
+
+const getUpdatedHistory = (
+  searchWord: string,
+  history: string[],
+  limit = 8
+) => {
+  // 히스토리에 검색어가 있는 경우, 맨 앞으로 이동
+  if (history.includes(searchWord)) {
+    return [
+      searchWord,
+      ...history.filter((item: string) => item !== searchWord),
+    ];
+  }
+  // limit 제한
+  if (history.length === limit) {
+    return [searchWord, ...history.slice(1, limit - 1)];
+  }
+
+  return [searchWord, ...history];
+};
+
+interface Props {
+  handleSearch: (value: string) => void;
+  placeholder: string;
+}
+
+export default function Search({ handleSearch, placeholder }: Props) {
+  const searchValueRef = useRef<HTMLInputElement>(null);
+  const historyRef = useRef<HTMLDivElement>(null);
+  const [history, setHistory] = useState<string[]>(
+    typeof window !== "undefined"
+      ? LocalStorageUtility.getItem<string[]>(SEARCH_HISTORY, [])
+      : []
+  );
+  const [isHistoryShown, setIsHistoryShown] = useState<boolean>(false);
+
+  /**
+   * 검색 후 실행 함수
+   * localStorage, state에 검색기록 저장하고, 검색기록 창 닫음
+   * @param searchWord 검색어
+   */
+  const saveSearchHistory = (searchWord: string) => {
+    const updatedHistory = getUpdatedHistory(searchWord, history);
+    LocalStorageUtility.setItem(SEARCH_HISTORY, updatedHistory);
+    setHistory(updatedHistory);
+    setIsHistoryShown(false);
+  };
+
+  /**
+   * 검색창 KeyDownEventHandler
+   * @description searchValueRef 값을 읽고, 값이 있는 경우 검색 실행
+   */
+  const onKeyDownInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const searchWord = searchValueRef.current?.value;
+    const { key } = e;
+    // 엔터 시 검색 실행
+    if (searchWord !== "" && searchWord && key === "Enter") {
+      handleSearch(searchWord);
+      saveSearchHistory(searchWord);
+    }
+  };
+
+  /**
+   * 검색창 FocusEventHandler
+   * @description 검색창 foucs 시 검색기록 노출
+   */
+  const onFocusInput = () => {
+    setIsHistoryShown(true);
+  };
+
+  /**
+   * 검색창 ChangeEventHandler
+   * @description 검색창에 값이 없는 경우 검색기록 노출
+   */
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value === "") {
+      setIsHistoryShown(true);
+    }
+  };
+
+  /**
+   * 검색기록 클릭 시, 검색 실행
+   * @param historyWord
+   */
+  const onClickHistory = (historyWord: string) => {
+    handleSearch(historyWord);
+    saveSearchHistory(historyWord);
+  };
+
+  /**
+   * 검색기록 X 버튼 클릭 시, 검색기록 삭제
+   * @param deleteValue
+   */
+  const deleteHistory = (deleteValue: string) => {
+    const updatedHistory = history.filter(
+      (item: string) => item !== deleteValue
+    );
+    LocalStorageUtility.setItem(SEARCH_HISTORY, updatedHistory);
+    setHistory(updatedHistory);
+  };
+
+  useClickOutside(historyRef, () => {
+    setIsHistoryShown(false);
+  });
+
+  return (
+    <div
+      ref={historyRef}
+      className="relative w-[480px] h-[50px] pl-[10px] pr-[26px] bg-lightGray rounded-[100px] flex items-center gap-[10px] z-[1]"
+    >
+      <SearchIcon />
+      <input
+        data-input="overall"
+        className="left-[50px] focus:outline-none flex-1 placeholder-txt-subtitle"
+        ref={searchValueRef}
+        placeholder={placeholder}
+        onFocus={onFocusInput}
+        onKeyDown={onKeyDownInput}
+        onChange={onChangeInput}
+      />
+      {/** history */}
+      {isHistoryShown && history.length > 0 && (
+        <div className="absolute top-[0px] left-0 bg-lightGray w-full rounded-t-[28px] rounded-b-[24px] flex flex-col items-center z-[-1] text-txt-subtitle">
+          <div className="h-[50px]" />
+          {history?.map((item: string) => (
+            <div
+              key={item}
+              className="relative w-full py-[12px] flex items-center"
+            >
+              <button
+                className="w-full ml-[52px] mr-[50px] text-left whitespace-nowrap overflow-x-hidden"
+                onClick={() => onClickHistory(item)}
+              >
+                {item}
+              </button>
+              <button
+                className="absolute right-[30px]"
+                onClick={() => deleteHistory(item)}
+              >
+                <CancelIcon className="text-black w-[20px] h-[20px]" />
+              </button>
+            </div>
+          ))}
+          <div className="h-[14px]" />
+        </div>
+      )}
+    </div>
+  );
+}
