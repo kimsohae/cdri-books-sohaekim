581ae147320d0b5cbcdb4ba60c8e12227b746918
sohaekim <sohaekim1@gmail.com>
2025-04-16 13:53:28 +0900

    update: config VITE_API_URL ->VITE_KAKAO_API_URL

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_KAKAO_API_KEY;
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_KAKAO_API_URL;
 
 if (!(API_KEY && API_BASE_URL)) {
     throw new Error("Missing env! env를 확인해 주세요.");
